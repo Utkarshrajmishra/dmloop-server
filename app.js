97a6aa29aad46eaaf5c0f69848e624a04dc4585f
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2025-02-15 01:18:58 +0530

    Fix: Path

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://dmloop.vercel.app/",
+    origin: "https://keyboardwars.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
